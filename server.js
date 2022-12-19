69b284194135dd8051259436c3eb4408b3ebb99b
jonathan-crepeau <jon.crepeau@gmail.com>
2022-12-19 11:20:38 -0800

    first fastify attempt not successful (code)

--- server.js
+++ server.js
@@ -6,6 +6,9 @@ import express from 'express';
 const app = express();
 const PORT = process.env.PORT;
 
+const Fastify = require('fastify');
+const server = Fastify();
+
 import { router as viewsRouter } from './routes/views';
 import { router as tokenRouter } from './routes/token';
 
@@ -29,6 +32,18 @@ app.use('/token', tokenRouter);
 // SECTION Proxy Server
 const CLIENT_ID = process.env.API_KEY;
 const SECRET = process.env.API_SECRET;
+
+server.register(require('@fastify/http-proxy'), {
+    upstream: 'http://my-api.example.com',
+    prefix: '/token',
+    undici: true,
+    replyOptions: {
+        body: `grant_type=client_credentials&client_id=${CLIENT_ID}&client_secret=${SECRET}`
+    }
+})
+
+server.listen({ port: 3000 });
+
 
 // SECTION - Server Start
 app.listen(PORT, () => console.log(`App listening on ${PORT}..`));
