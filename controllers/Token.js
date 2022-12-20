ce5126f90ff08068a7ab2004ae7d1d19fb2e1d36
jonathan-crepeau <jon.crepeau@gmail.com>
2022-12-19 16:51:52 -0800

    successful token route that retrieves env varibles for clientside use

--- controllers/Token.js
+++ controllers/Token.js
@@ -1,10 +1,6 @@
+
 const root = (req, res) => {
-    fetch('https://api.petfinder.com/v2/oauth2/token', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/x-www-form-urlencoded'
-        }
-    });
-};
+    res.json({Client_Id: process.env.API_KEY, Client_Secret: process.env.API_SECRET})
+}
 
 export { root }
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -1,13 +1,27 @@
 console.log('app.js reporting for duty..');
 
-async function fetchToken() {
-    await fetch('http://localhost:3000/token', {
+const useCredentials = () => {
+    fetch('http://localhost:3007/token', {
         method: 'GET'
     })
         .then((response) => response.json())
-        .then((data) => console.log(data))
+        .then((data) => getToken(data))
         .catch((error) => console.log(error));
 };
 
-fetchToken();
+const getToken = (resData) => {
+    // console.log(resData.Client_Id)
+    fetch('https://api.petfinder.com/v2/oauth2/token', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: `grant_type=client_credentials&client_id=${resData.Client_Id}&client_secret=${resData.Client_Secret}`
+    })
+        .then((response) => response.json())
+        .then((data) => console.log(data.access_token))
+        .catch((error) => console.log(error));
+};
+
+useCredentials();
 
